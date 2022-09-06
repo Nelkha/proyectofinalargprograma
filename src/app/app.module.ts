aadbd647626f45252dcaf84580fbac115c537f49
Nelkha <maritogallego@hotmail.com>
2022-09-06 19:51:53 -0300

    hask

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgCircleProgressModule } from 'ng-circle-progress';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SupBarComponent } from './components/sup-bar/sup-bar.component';
 import { AboutComponent } from './components/about/about.component';
 import { LaboralComponent } from './components/laboral/laboral.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
+import { HaskComponent } from './components/hask/hask.component';
 
 
 
@@ -17,11 +18,22 @@ import { EducacionComponent } from './components/educacion/educacion.component';
     SupBarComponent,
     AboutComponent,
     LaboralComponent,
-    EducacionComponent
+    EducacionComponent,
+    HaskComponent
     
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgCircleProgressModule.forRoot({
+      // set defaults here
+      radius: 100,
+      outerStrokeWidth: 16,
+      innerStrokeWidth: 8,
+      outerStrokeColor: "#78C000",
+      innerStrokeColor: "#C7E596",
+      animationDuration: 300,
+    
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/hask/hask.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HaskComponent } from './hask.component';
+
+describe('HaskComponent', () => {
+  let component: HaskComponent;
+  let fixture: ComponentFixture<HaskComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HaskComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HaskComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
